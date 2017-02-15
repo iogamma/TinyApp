a00e61cef8377aa4d676e8d95183278c366fe5a6
Hal Tang <hal.wh.tang@gmail.com>
2017-02-15 23:27:44 +0000

    added update functionality and refactored code

--- express_server.js
+++ express_server.js
@@ -8,46 +8,6 @@ const fs = ('fs');
 const app = express();
 const urlDatabase = {};
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-// Routes
-
-app.get('/', (req, res) => {
-  res.render('urls_index');
-});
-
-app.get('/urls', (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render('urls_new');
-});
-
-app.get("/u/:shortURL", (req, res) => {
-  longURL = urlDatabase[req.params.shortURL];
-  if (urlDatabase[req.params.shortURL]) {
-    res.redirect(longURL);
-  } else {
-    console.log("The short URL does not exist in the database.");
-  }
-});
-
-app.post("/urls/:shortURLToDel/delete", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  delete urlDatabase[req.params.shortURLToDel];
-  res.render('urls_index', templateVars);
-});
-app.post("/urls", (req, res) => {
-  urlDatabase[`${generateRandStr()}`] = `http://${req.body.longURL}`;
-  res.redirect('/urls');
-});
-
-
-
 function generateRandStr() {
   let randNum;
   let randString = "";
@@ -62,6 +22,62 @@ function generateRandStr() {
 
   return randString;
 }
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Get Routes
+
+app.get('/', (req, res) => {
+  res.render('urls_index');
+});
+
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
+app.get('/urls/:id', (req, res) => {
+  let theID = req.params.id;
+  let templateVars = { urls: urlDatabase,
+                       id: theID };
+  switch(theID) {
+    case 'new':
+      res.render('urls_new');
+      break;
+    default:
+      res.render('urls_show', templateVars);
+  };
+})
+
+app.get("/u/:shortURL", (req, res) => {
+  longURL = urlDatabase[req.params.shortURL];
+  if (urlDatabase[req.params.shortURL]) {
+    res.redirect(longURL);
+  } else {
+    console.log("The short URL does not exist in the database.");
+  }
+});
+
+// Post Routes
+
+app.post("/urls/:shortURLToDel/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURLToDel];
+  res.redirect('/urls');
+});
+
+app.post("/urls/:id", (req, res) => {
+  let theID = req.params.id;
+  switch(theID) {
+    case 'new':
+      urlDatabase[`${generateRandStr()}`] = `http://${req.body.longURL}`;
+      res.redirect('/urls');
+      break;
+    default:
+      urlDatabase[theID] = `http://${req.body.newlongURL}`;
+      res.redirect(`/urls`);
+  };
+});
 
 app.listen(8080);
 console.log('TinyApp server running.');
