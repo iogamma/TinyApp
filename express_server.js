3fa37a689bc4eb5ce104ce5ce869eaa7fdf4da01
Hal Tang <hal.wh.tang@gmail.com>
2017-02-15 03:53:37 +0000

    Form page added and random 6 alphanumeric generator implemented

--- express_server.js
+++ express_server.js
@@ -1,14 +1,15 @@
-// server.js
+/* express_server.js */
 // load the things we need
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-// set the view engine to ejs
+const urlDatabase = {};
+
 app.set('view engine', 'ejs');
 
-let urlDatabase = {};
+app.use(bodyParser.urlencoded({extended: true}));
 
-// index page
 app.get('/', (req, res) => {
   res.render('index');
 });
@@ -18,10 +19,32 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-app.get("/urls/:id", (req, res) => {
-  let templateVars = { shortURL: req.params.id };
-  res.render('urls_show', templateVars);
+// app.get("/urls/:id", (req, res) => {
+//   let templateVars = { shortURL: req.params.id };
+//   res.render('urls_show', templateVars);
+// });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+function generateRandomString() {
+  let randNum;
+  let randString = "";
+
+  for (let i = 0; i < 6; i++) {
+    randNum = Math.floor(Math.random() * 36) + 48;
+    randNum > 57 ? randNum += 39 : null ;
+    randString += String.fromCharCode(randNum);
+  }
+
+  return randString;
+}
+
 app.listen(8080);
-console.log('8080 is the magic port');
+console.log('TinyApp server running.');
